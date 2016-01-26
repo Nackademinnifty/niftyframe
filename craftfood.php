69c1a79270d97a19d820f3e9a09bc3518a626466
mihini <manina103@hotmail.com>
2016-01-26 18:00:56 +0100

    mobile layout done

--- craftfood.php
+++ craftfood.php
@@ -5,23 +5,29 @@ $pageId = 'Craftfood';
 
 
     <?php include 'incl/header.php'?>
+       <div class="row">
 
         <section class="project">
             <article>
+               <div class="col-xs-12 col-md-4">
                 <h2>Client: <span>Craftfood</span></h2>
                 <h2>Project: <span>New website</span></h2>
                 <h2>Link:<a href="#"> Craftfood.se</a></h2> 
                 <h2>Description:</h2>
+                
                 <p>Craftfood is a restaurant that started 2015 in Stockholm. They needed a new webpage with information about their food and philosophy.</p>
                 <p> The webpage is responsive and works on every screen size even on mobiles. You can even make reservations on the page..</p>
 
-                <p> We made apps both for android and iOS. The website is also responsive which means that the members doesn’t need to download the app to surf the site.</p>
+                <p> We made apps both for android and iOS. The website is also responsive which means that the members doesn’t need to download the app to use the functions.</p>
+               </div>
                 
-                <figure>
-                    <a target="_top" href="img/work/craftfood.jpg"><img src="img/work/craftfood.jpg" alt="craftfood"></a>
+                <figure class="col-xs-12 col-md-4">
+                    <a target="_top" href="img/work/craftfood.jpg"><img class="img-responsive" src="img/work/craftfood.jpg" alt="craftfood Responsive image"></a>
                 </figure>
             </article>
         </section>
+			</div>
+        
 
 
         <?php include 'incl/footer.php'?>
