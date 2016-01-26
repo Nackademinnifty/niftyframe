e9836cd992c8a822a24314a483f14566f0241803
mihini <manina103@hotmail.com>
2016-01-26 19:20:15 +0100

    added training365 page

---
+++ training365.php
@@ -0,0 +1,28 @@
+<?php
+$pageTitle = 'Training365';
+$pageId = 'Training365';
+?>
+
+
+    <?php include 'incl/header.php'?>
+
+        <section class="project row">
+            <article>
+               <div class="col-xs-12 col-md-5">
+               	<h2>Client: <span>Training 365</span></h2>
+                <h2>Project: <span> New site & App</span></h2>
+                <h2>Link:<a href="#">Training 365</a></h2> 
+                <h2>Description:</h2>
+                <p>Traning 365 wanted us to make a site and app for their members so that are easier for them to book group training. The site will also give inspirational and motivational articles for the members to workout more.</p>
+
+                <p> We made apps both for android and iOS. The website is also responsive which means that the members doesn’t need to download the app to surf the site.</p>
+							</div>
+                
+                <figure class="pull-right col-xs-12 col-md-6">
+                    <a target="_top" href="img/work/training.jpg"><img class="img-responsive" src="img/work/training.jpg" alt="training365 page Responsive image"></a>
+                </figure>
+            </article>
+        </section>
+
+
+        <?php include 'incl/footer.php'?>
