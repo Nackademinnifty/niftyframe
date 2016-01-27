30b75295d82cea35122084160e3e040a406f3d13
Victor Trigo Wagner <Victor Trigo Wagner>
2016-01-27 10:02:13 +0100

    BS about

--- about.php
+++ about.php
@@ -3,74 +3,167 @@ $pageTitle = 'About Nifty';
 $pageId = 'about';
 ?>
 
-
 	<?php include 'incl/header.php'?>
-		<h1 id="foo"></h1>
 
 
-		<div class="aboutIntro" id="aboutTeam">
-			<h2>We work here!</h2>
-			<p>We're a small company of four hightly driven och skilled web-developers</p>
-		</section>
 
-		<section class="employeeWrap clear">
-			<div class="employeeImage" id="victor"></div>
-			<div class="employeeDesc">
-				<div class="employeeName">
-					<h3>Name</h3>
-					<h2>Victor Trigo Wagner</h2><hr /></div>
-				<div class="employeeProfile">
+
+
+
+
+
+
+
+
+</div>
+
+			<section class="row aboutIntro col-md-12 text-center center-block" id="aboutTeam">
+				<h1>We are Nifty!</h1>
+				<p>We're a small company of four hightly driven och skilled web-developers</p>
+			</section>
+
+
+
+
+
+
+				<section class="row employee col-xs-12 col-sm-6 col-md-3 center-block">
+					<div class="employeeImage col-sm-12 img-circle col-center-block" id="nina"></div>
+					<div class="employeeDesc text-center">
+						<div class="employeeName col-sm-12">
+							<h3>Name</h3>
+							<h4>Nina Ma</h4>
+						</div>
+						<div class="employeeTitle col-sm-12">
+							<h3>Title</h3>
+							<h4>Front-end developer</h4>
+						</div>
+						<div class="employeeQuote col-sm-12">
+							<h3>Quote</h3>
+							<h4>"I don't know why people eat so badly. I could eat pasta all the time, but it really is fattening. And I love ice cream, but I can't do that. There was a time, until I was in my mid-forties, when I could eat a whole pizza - and really, no effect."</h4>
+						</div>
+					</div>
+				</section>
+
+
+				<section class="row employee col-xs-12 col-sm-6 col-md-3 center-block">
+					<div class="employeeImage col-sm-12 img-circle col-center-block" id="robin"></div>
+					<div class="employeeDesc text-center">
+						<div class="employeeName col-sm-12">
+							<h3>Name</h3>
+							<h4>Robin Zetterström</h4>
+						</div>
+						<div class="employeeTitle col-sm-12">
+							<h3>Title</h3>
+							<h4>Front-end developer</h4>
+						</div>
+						<div class="employeeQuote col-sm-12">
+							<h3>Quote</h3>
+							<h4>"I don't know why people eat so badly. I could eat pasta all the time, but it really is fattening. And I love ice cream, but I can't do that. There was a time, until I was in my mid-forties, when I could eat a whole pizza - and really, no effect."</h4>
+						</div>
+					</div>
+				</section>
+
+				<section class="row employee col-xs-12 col-sm-6 col-md-3 center-block">
+					<div class="employeeImage col-sm-12 img-circle col-center-block" id="joel"></div>
+					<div class="employeeDesc text-center">
+						<div class="employeeName col-sm-12">
+							<h3>Name</h3>
+							<h4>Joel Sjöström</h4>
+						</div>
+						<div class="employeeTitle col-sm-12">
+							<h3>Title</h3>
+							<h4>Front-end developer</h4>
+						</div>
+						<div class="employeeQuote col-sm-12">
+							<h3>Quote</h3>
+							<h4>"I don't know why people eat so badly. I could eat pasta all the time, but it really is fattening. And I love ice cream, but I can't do that. There was a time, until I was in my mid-forties, when I could eat a whole pizza - and really, no effect."</h4>
+						</div>
+					</div>
+				</section>
+
+				<section class="row employee col-xs-12 col-sm-6 col-md-3 center-block">
+					<div class="employeeImage col-sm-12 img-circle col-center-block" id="victor"></div>
+					<div class="employeeDesc text-center">
+						<div class="employeeName col-sm-12">
+							<h3>Name</h3>
+							<h4>Victor Trigo Wagner</h4>
+						</div>
+						<div class="employeeTitle col-sm-12">
+							<h3>Title</h3>
+							<h4>Front-end developer</h4>
+						</div>
+						<div class="employeeQuote col-sm-12">
+							<h3>Quote</h3>
+							<h4>"I don't know why people eat so badly. I could eat pasta all the time, but it really is fattening. And I love ice cream, but I can't do that. There was a time, until I was in my mid-forties, when I could eat a whole pizza - and really, no effect."</h4>
+						</div>
+					</div>
+				</section>
 				</div>
-				<div class="employeeTitle">
-					<h3>Title</h3>
-					<h2>Front-end developer</h2><hr />
-				</div>
-				<div class="employeeQuote clear">
-					<h3>Quote</h3>
-					<h4>"Hello, I'm Dr. Bean. Apparently. And my job is to sit and look at paintings. So, what have I learned that I can say about this painting? Well, firstly, it's really quite big, which is excellent. because If it were really small, you know, microscopic, then hardly anyone would be able to see it."</h4></div>
+
+<div class="clearfix">
+
+</div>
+
+			<div class="row aboutIntro col-md-12 text-center center-block" id="aboutOffers">
+				<h1>What we offer!</h1>
+				<p>Highly customizable, responsive, mobile-friendly websites that help your business grow!</p>
 			</div>
-		</section>
 
-		<section class="employeeWrap clear">
-			<div class="employeeImage" id="robin"></div>
-			<div class="employeeDesc">
-				<div class="employeeName">
-					<h3>Name</h3>
-					<h2>Christopher Walken</h2><hr /></div>
-				<div class="employeeProfile">
-				</div>
-				<div class="employeeTitle">
-					<h3>Title</h3>
-					<h2>Front-end developer</h2><hr />
-				</div>
-				<div class="employeeQuote clear">
-					<h3>Quote</h3>
-					<h4>"I don't know why people eat so badly. I could eat pasta all the time, but it really is fattening. And I love ice cream, but I can't do that. There was a time, until I was in my mid-forties, when I could eat a whole pizza - and really, no effect."</h4></div>
+
+			<div class="row col-md-12 text-center center-block" id="offerWrap">
+				<ul class="list-unstyled">
+					<li class="fa fa-bolt"></li>
+					<br />
+					<li class="fa fa-bolt"></li>
+					<br />
+					<li class="fa fa-bolt"></li>
+					<br />
+					<li class="fa fa-bolt"></li>
+					<br />
+					<li class="fa fa-bolt"></li>
+				</ul>
 			</div>
-		</section>
+
+			<div class="row col-md-12 aboutIntro text-center center-block" id="aboutAwards">
+				<h1>What we have won!</h1>
+				<p>With hard work and determination we have been awarded these fine prizes!</p>
+			</div>
+
+			<div class="row col-md-12 text-center center-block" id="awardsWrap">
+				<ul class="list-unstyled">
+					<li class="fa fa-trophy"> Most awesome company 2016</li>
+					<br />
+					<li class="fa fa-trophy"> Most beautiful staff</li>
+					<br />
+					<li class="fa fa-trophy"> 4W€$0M€ CODERS</li>
+					<br />
+					<li class="fa fa-trophy"> This and that</li>
+				</ul>
+			</div>
 
 
-		<div class="aboutIntro" id="aboutOffers">
-			<h2>What we offer!</h2>
-			<p>Highly customizable, responsive, mobile-friendly websites that help your business grow!</p>
-		</section>
+			<div class="row col-md-12 aboutIntro text-center center-block" id="aboutPhilosophy">
+				<h1>What we belive in!</h1>
+				<p>With great customers comes great responsibility!</p>
+			</div>
 
-		<div class="aboutIntro" id="aboutAwards">
-			<h2>What we have won!</h2>
-			<p>With hard work and determination we have been awarded theese fine prizes!</p>
-		</section>
+			<div class="row col-md-12 text-center center-block" id="philosophyWrap">
+				<ul class="list-unstyled">
+					<li class="fa fa-check"> Deliver on time</li>
+					<br />
+					<li class="fa fa-check"> Quality built in</li>
+					<br />
+				</ul>
+			</div>
 
-		<div class="aboutIntro" id="aboutPhilosophy">
-			<h2>What we belive in!</h2>
-			<p>With great customers comes great responsibility!</p>
-		</section>
+			<div class="row col-md-12 aboutIntro text-center center-block" id="aboutHistory">
+				<h1>Where we come from!</h1>
+				<p>Four highly motivated students who decided to bring true beauty to the web!</p>
+			</div>
+			<div id="historyWrap">
 
-		<div class="aboutIntro" id="aboutHistory">
-			<h2>Where we come from!</h2>
-			<p>Four highly motivated students who decided bring true beauty to the web!</p>
-		</section>
-
-
-
+			</div>
+		</div>
 
 		<?php include 'incl/footer.php'?>
